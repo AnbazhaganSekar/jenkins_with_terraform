9f0de34503799c7bae6b75933badf9cb786cdb71
Anbazhagan S <34671765+AnbazhaganSekar@users.noreply.github.com>
2023-08-09 09:47:48 +0530

    last  main.tf
    last commit

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ access_key = "AKIA2OPA7DHITUV2O2GS"
 secret_key = "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"
 }
  #vpc.tf
-
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
