9f8086384617a40ba0b15a65154773cbd8a2ec25
Anbazhagan S <34671765+AnbazhaganSekar@users.noreply.github.com>
2023-08-09 09:32:48 +0530

    ec2 updates main.tf
    2 numbers ec2 instances created in the ap-south-1a region

--- main.tf
+++ main.tf
@@ -16,3 +16,16 @@ resource "aws_vpc" "main" {
     Purpose = "terrafrom using Jenkins"
   }
 }
+#ec2 creation.tf
+
+# Create 2 EC2 Instance
+resource "aws_instance" "web" {
+  ami           = "ami-079b5e5b3971bd10d" # Amazon Linux
+  availability_zone = "ap-south-1a"
+  instance_type = "t2.micro"
+  count         = 2
+  tags = {
+    #"Name" = "web"
+    "Name" = "web-${count.index}"
+  }
+}
